2041211e9e105efd88b56be7b8fc9db77cd82661
謝昕芳 <s1133324@mail.yzu.edu.tw>
2026-05-02 16:15:27 +0800

    使用自定義函式寫程式

--- poker/frmPicTest.cs
+++ poker/frmPicTest.cs
@@ -17,13 +17,25 @@ namespace poker
             InitializeComponent();
         }
 
+        #region 自定義方法
+        // 用字串讀取圖片
+        private Image GetImage(string name)
+        {
+            return Properties.Resources.ResourceManager.GetObject(name) as Image;
+        }
+        // 用撲克牌編號讀取圖片
+        private Image GetImage(int num)
+        {
+            return GetImage($"pic{num}");
+        }
+
+        #endregion
         private void btnTest_Click(object sender, EventArgs e)
         {
             Random random = new Random();
             int r = random.Next(1, 53);//(a,b)-> a >= 1, b < 53
-            picTest.Image = Properties.Resources.
-            ResourceManager.GetObject($"pic{r}")
-            as Image;
+
+            picTest.Image = GetImage(r);
             lblNum.Text = r.ToString();
 
         }
