042d9729fe3a4e2c4c92e7a9c9579187309f4c24
謝昕芳 <s1133324@mail.yzu.edu.tw>
2026-05-02 18:20:45 +0800

    新增撲克牌遊戲基本 UI 與發牌功能
    - 新增 grpButton 群組，包含發牌、換牌、判斷牌型按鈕及結果標籤
    - 設定表單大小，初始化新控制項並綁定事件
    - 新增 PictureBox 陣列與玩家手牌、牌組資料結構
    - 實作 pic_Click 事件，顯示選擇的牌
    - 新增 Shuffle() 洗牌方法
    - 新增 btnDealCard_Click 非同步發牌流程，支援洗牌、發牌與互動
    - 為 UI 增加發牌、換牌、判斷牌型等基本功能框架

--- poker/frmPoker.Designer.cs
+++ poker/frmPoker.Designer.cs
@@ -29,6 +29,12 @@
         private void InitializeComponent()
         {
             this.grpPoker = new System.Windows.Forms.GroupBox();
+            this.grpButton = new System.Windows.Forms.GroupBox();
+            this.btnDealCard = new System.Windows.Forms.Button();
+            this.btnChangeCard = new System.Windows.Forms.Button();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.grpButton.SuspendLayout();
             this.SuspendLayout();
             // 
             // grpPoker
@@ -37,21 +43,79 @@
             this.grpPoker.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.grpPoker.Location = new System.Drawing.Point(35, 35);
             this.grpPoker.Name = "grpPoker";
-            this.grpPoker.Size = new System.Drawing.Size(504, 161);
+            this.grpPoker.Size = new System.Drawing.Size(504, 163);
             this.grpPoker.TabIndex = 0;
             this.grpPoker.TabStop = false;
             this.grpPoker.Text = "牌桌";
+            // 
+            // grpButton
+            // 
+            this.grpButton.Controls.Add(this.lblResult);
+            this.grpButton.Controls.Add(this.btnCheck);
+            this.grpButton.Controls.Add(this.btnChangeCard);
+            this.grpButton.Controls.Add(this.btnDealCard);
+            this.grpButton.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.grpButton.Location = new System.Drawing.Point(35, 213);
+            this.grpButton.Name = "grpButton";
+            this.grpButton.Size = new System.Drawing.Size(504, 80);
+            this.grpButton.TabIndex = 0;
+            this.grpButton.TabStop = false;
+            this.grpButton.Text = "功能";
+            // 
+            // btnDealCard
+            // 
+            this.btnDealCard.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnDealCard.Location = new System.Drawing.Point(24, 33);
+            this.btnDealCard.Name = "btnDealCard";
+            this.btnDealCard.Size = new System.Drawing.Size(85, 40);
+            this.btnDealCard.TabIndex = 0;
+            this.btnDealCard.Text = "發牌";
+            this.btnDealCard.UseVisualStyleBackColor = false;
+            this.btnDealCard.Click += new System.EventHandler(this.btnDealCard_Click);
+            // 
+            // btnChangeCard
+            // 
+            this.btnChangeCard.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnChangeCard.Enabled = false;
+            this.btnChangeCard.Location = new System.Drawing.Point(115, 33);
+            this.btnChangeCard.Name = "btnChangeCard";
+            this.btnChangeCard.Size = new System.Drawing.Size(88, 40);
+            this.btnChangeCard.TabIndex = 1;
+            this.btnChangeCard.Text = "換牌";
+            this.btnChangeCard.UseVisualStyleBackColor = false;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnCheck.Enabled = false;
+            this.btnCheck.Location = new System.Drawing.Point(209, 33);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(102, 40);
+            this.btnCheck.TabIndex = 2;
+            this.btnCheck.Text = "判斷牌型";
+            this.btnCheck.UseVisualStyleBackColor = false;
+            // 
+            // lblResult
+            // 
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResult.Location = new System.Drawing.Point(317, 37);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(171, 33);
+            this.lblResult.TabIndex = 1;
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // frmPoker
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(720, 295);
+            this.ClientSize = new System.Drawing.Size(721, 355);
+            this.Controls.Add(this.grpButton);
             this.Controls.Add(this.grpPoker);
             this.Name = "frmPoker";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "五張撲克牌";
+            this.grpButton.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -60,5 +124,10 @@
         #endregion
 
         private System.Windows.Forms.GroupBox grpPoker;
+        private System.Windows.Forms.GroupBox grpButton;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Button btnChangeCard;
+        private System.Windows.Forms.Button btnDealCard;
     }
 }
--- poker/frmPoker.cs
+++ poker/frmPoker.cs
@@ -12,7 +12,12 @@ namespace poker
 {
     public partial class frmPoker : Form
     {
+        #region 欄位
         PictureBox[] pic = new PictureBox[5];
+        int[] allPoker = new int[52];
+        int[] playerPoker = new int[5];
+
+        #endregion
         public frmPoker()
         {
             InitializeComponent();
@@ -32,12 +37,19 @@ namespace poker
                 pic[i].Top = 30;
                 pic[i].Left = 10 + ((pic[i].Width + 10) * i);
                 pic[i].Visible = true;
+                pic[i].Enabled = false;
+                pic[i].Tag = "back";
                 // 將 pic 丟至到 grpPorker 內
                 this.grpPoker.Controls.Add(pic[i]);
-                //pic[i].MouseClick += new MouseEventHandler(pic_Click);
+                pic[i].MouseClick += new MouseEventHandler(pic_Click);
             }
         }
+        private void pic_Click(object sender, MouseEventArgs e)
+        {
+            PictureBox pic = (PictureBox)sender;
+            MessageBox.Show("你選擇了" + pic.Name);
 
+        }
         private Image GetImage(string name)
         {
             return Properties.Resources.ResourceManager.GetObject(name) as Image;
@@ -48,5 +60,50 @@ namespace poker
             return GetImage($"pic{num}");
         }
         #endregion
+
+        #region 洗牌
+        private void Shuffle()
+        {
+            Random rand = new Random();
+            for (int i = 0; i < allPoker.Length; i++)
+            {
+                int r = rand.Next(allPoker.Length);
+                int temp = allPoker[r];
+                allPoker[r] = allPoker[0];
+                allPoker[0] = temp;
+            }
+        }
+        #endregion
+        private async void btnDealCard_Click(object sender, EventArgs e)
+        {
+            // 先將牌面蓋掉
+            for (int i = 0; i < 5; i++)
+            {
+                pic[i].Image = GetImage("back");
+            }
+            // 初始化52張牌
+            for (int i = 0; i < 52; i++)
+            {
+                allPoker[i] = i;
+            }
+            // 洗牌
+            Shuffle();
+            // 暫停500ms
+            await Task.Delay(500);
+
+            // 發牌
+            for (int i = 0; i < 5; i++)
+            {
+                pic[i].Image = GetImage("pic" + (allPoker[i] + 1));
+                playerPoker[i] = allPoker[i];
+            }
+            // 啟用所有牌的點擊事件
+            for (int i = 0; i < 5; i++)
+            {
+                pic[i].Enabled = true;
+                pic[i].Tag = "front";
+            }
+            btnChangeCard.Enabled = true;
+        }
     }
 }
